207579bd4b5dfb8265be33de397a86058134c392
livia <liviamajzoubsilva19@gmail.com>
2025-04-17 15:28:18 -0300

    Update while_9.c

--- while_9.c
+++ while_9.c
@@ -13,7 +13,7 @@ int main() {
 
     printf("Numeros de 1 a 200 que divididos por %d dao resto 3 e sao pares:\n", n);
     
-    for (int i = 1; i <= 200; i++) {
+    if (int i = 1; i <= 200; i++) {
         if (i % n == 3 && i % 2 == 0) {
         printf("%d\n", i);
         }
